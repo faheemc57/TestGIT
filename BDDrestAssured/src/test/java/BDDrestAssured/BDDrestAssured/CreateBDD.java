cd61de8fba7e4f8689393ab88036743ead9c8016
USER <USER@DESKTOP-EJQ0QFQ>
2019-02-13 15:27:09 +0530

    1st push

---
+++ BDDrestAssured/src/test/java/BDDrestAssured/BDDrestAssured/CreateBDD.java
@@ -0,0 +1,12 @@
+package BDDrestAssured.BDDrestAssured;
+
+public class CreateBDD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+
+	}
+
+}
